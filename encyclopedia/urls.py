995c4fed98b556e4066bb583d31a94ffdbc0f5ed
Esteban <serafa99@hotmail.com>
2022-03-18 16:56:28 -0300

    Recibimos una palabra  en la url y la guardamos

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    # Al escribir algo en la dirección url, se envía lo escrito a views con el nombre "name"     
+    path("<str:name>", views.buscar, name="buscar"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -7,4 +7,8 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+# Aqui definimos la función "buscar". Por ahora nos envía a una página de prueba
+# Ahora le vamos a pasar un argumento
+def buscar(request, name):
+    return render(request, "encyclopedia/prueba.html", {"name":name})
 
