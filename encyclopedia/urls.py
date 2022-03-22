c6ff72b91d93ede39a23e3d74112a46f526ba8a7
Esteban <serafa99@hotmail.com>
2022-03-21 21:33:23 -0300

    Ya funciona el botón de búsqueda

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,8 +2,20 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    # Al escribir algo en la dirección url, se envía lo escrito a views con el nombre "name"     
-    path("<str:name>", views.buscar, name="buscar"),
+
+    # Al escribir algo en la dirección url, se envía lo escrito a views con el nombre "name"  
+      
+    path("wiki/<str:name>", views.buscar, name="buscar"),
+
+    # De que forma enviar a views.recibir? Siempre va a views.buscar, porque es muy generico
+    path("recibir", views.recibir, name="recibir"),
+
+    # Vamos a crear un path para una aplicacion de prueba
+
+    path("prueba", views.prueba, name="prueba"),
+
+    path("formulario", views.formulario, name="formulario")
 ]
